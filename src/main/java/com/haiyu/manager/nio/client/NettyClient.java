e0e3ecf5c310dd4f39e3ac39616a6f9bd12ed2c3
zhongtao wang <853306121@qq.com>
2019-09-02 18:26:46 +0800

    netty

---
+++ src/main/java/com/haiyu/manager/nio/client/NettyClient.java
@@ -0,0 +1,40 @@
+package com.haiyu.manager.nio.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * @ Author     : wzt.
+ * @ Description: Netty Client 类
+ * @ Date       : Created in 19-9-2
+ * @ Modified By:
+ * @ Version    : v1.0
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
